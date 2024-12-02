1790f6967971bb42ab58e65bc1858e125927f172
Jake Eckhardt <jake.c.eckhardt@gmail.com>
2024-12-01 23:31:09 -0600

    More and more testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,8 @@ export default async function Home() {
   const cookieStore = cookies();
   const session = cookieStore.get('session')!.value;
 
+  console.log(session);
+
   const res = await fetch(`${url}/api/get-board`,{
     headers: {
       'Authorization': session
