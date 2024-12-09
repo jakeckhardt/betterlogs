9ef5dfcd5f76182c9a2d3b90a4e77bc024bc0447
Jake Eckhardt <jake.c.eckhardt@gmail.com>
2024-12-09 00:31:06 -0600

    Adding more console.log to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,12 @@ import { getUrl } from "./helpers/getUrl";
 import { cookies } from "next/headers";
 
 export default async function Home() {
-  console.log("Hit home page");
   const url = getUrl();
   const cookieStore = cookies();
   const session = cookieStore.get('session')!.value;
 
-  console.log("Making home page fetch");
+  console.log("Session token", session);
+  console.log(`Making home page fetch to ${url}/api/get-board`);
   const res = await fetch(`${url}/api/get-board`,{
     headers: {
       'Authorization': session
