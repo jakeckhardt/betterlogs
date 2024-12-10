8c10e3128c6c4f6a35eb9379224316368ce83bb9
Jake Eckhardt <jake.c.eckhardt@gmail.com>
2024-12-10 00:41:40 -0600

    Fixing preview environment variables

--- src/app/helpers/getUrl.ts
+++ src/app/helpers/getUrl.ts
@@ -1,10 +1,13 @@
 export const getUrl = () => {
     const env = process.env.NEXT_PUBLIC_VERCEL_ENV,
         url = process.env.NEXT_PUBLIC_VERCEL_URL,
+        previewUrl = process.env.VERCEL_BRANCH_URL,
         productionUrl = process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL;
 
     if (env === "production") {
         return `https://${productionUrl}`;
+    } else if (env === "preview") {
+        return `https://${previewUrl}`;
     } else {
         return `https://${url}`;
     }
