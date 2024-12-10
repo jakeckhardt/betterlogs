acad7a00112777bed18e91a947481c1e4e5c9fb5
Jake Eckhardt <jake.c.eckhardt@gmail.com>
2024-12-10 00:43:55 -0600

    Knew I would miss something with the system vars

--- src/app/helpers/getUrl.ts
+++ src/app/helpers/getUrl.ts
@@ -1,7 +1,7 @@
 export const getUrl = () => {
     const env = process.env.NEXT_PUBLIC_VERCEL_ENV,
         url = process.env.NEXT_PUBLIC_VERCEL_URL,
-        previewUrl = process.env.VERCEL_BRANCH_URL,
+        previewUrl = process.env.NEXT_VERCEL_BRANCH_URL,
         productionUrl = process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL;
 
     if (env === "production") {
